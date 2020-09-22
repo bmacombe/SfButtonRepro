5af4c8d642db1bac1cd577ae3a3fb71ae420e527
Brian Macomber <brian@agspectrum.com>
2020-09-22 16:59:44 -0500

    Add project files.

---
+++ SfButtonRepro/SfButtonRepro/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfButtonRepro
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void SfButton_OnClicked(object sender, EventArgs e)
+		{
+			Static.PreviousPage = Application.Current.MainPage;
+			Application.Current.MainPage = new MasterDetailPage1();
+		}
+	}
+}
---
+++ SfButtonRepro/SfButtonRepro/MasterDetailPage1Detail.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SfButtonRepro
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MasterDetailPage1Detail : ContentPage
+	{
+		public MasterDetailPage1Detail()
+		{
+			InitializeComponent();
+		}
+
+		private void SfButton_OnClicked(object sender, EventArgs e)
+		{
+			Application.Current.MainPage = Static.PreviousPage;
+		}
+	}
+}
